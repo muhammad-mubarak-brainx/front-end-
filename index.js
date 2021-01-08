39187334faf74137c02d1d7e83b05fdc94bc772d
Muhammad Mubarak <muhammad.mubarak@brainxtech.com>
2021-01-08 11:19:17 +0500

    modified changes and made dynamic cards working

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ loadDays = () => {
 }
 
 mealCount = 0;
-$(".add-button").bind("click", function () {
+$("body").on('click', ".add-button", function () {
+    console.log("I am clicked")
     title = $(this).closest(".card").data("title");
     titleImage = $(this).closest(".card").find("img").attr("src");
     mealsCounter(title, titleImage)
     appendCart(title, titleImage);
     mealCount++;
-    console.log(mealCount);
     if (mealCount === mealId) {
         $("#mealAlert").text('All Set');
         $('#submitCart').prop('disabled', false);
@@ -76,19 +76,17 @@ appendCart = (title, image) => {
     $("#col-second").append(container);
 }
 
-// cartFooter = () => {
-// footer = '<footer class="DrawerFooter-module__drawerFooter___3rxU2 w-100 px-2 px-md-3"><div class="row"><div class="DrawerFooter-module__instructionalCol___2kGEg d-none d-md-flex justify-content-between mb-2 py-2 col">        <button type="button" class="ClearAllBtn-module__clearAllBtn___2n7rl d-inline-block flex-shrink-0 py-0 px-2 px-md-0 text-uppercase text-decoration-none float-right btn btn-link"> clear all </button></div></div></footer>'
-// $("#col-second").append(footer);
-// }
-
 mealsCounter = (title, titleImage) => {
 
     if (Object.keys(mealArray).includes(title)) {
         count = mealArray[title].count
         console.log(count)
         mealArray[title] = { count: count + 1, titleImage: titleImage }
+        console.log(mealArray[title])
     } else {
+        console.log(" i am inside the else mealsCounter")
         mealArray[title] = { count: 1, titleImage: titleImage }
+        console.log(mealArray[title])
     }
 }
 
@@ -151,11 +149,14 @@ appendPriceList = () => {
 }
 
 appendMyMeals = () => {
+    console.log("i am inside the meal list loop")
     for (const key in mealArray) {
-        let myMealsList = '<li class="px-0 flex-shrink-0 list-group-item"><figure class="my-meals-item d-flex align-items-center mb-0"><div class="font-weight-500 text-center ml-2">' + mealArray[title].count + '</div><div> <img src="' + mealArray[title].titleImage + '"class="img-wrapper" ></div><div class="info-wrapper pl-4 pr-4 col">' + key + '</div></figure></li>'
-        console.log(myMealsList)
-        $("#checkoutMyMeals").append(myMealsList)
+        console.log(mealArray[key].titleImage)
+        let myMealsList = '<li class="px-0 flex-shrink-0 list-group-item"><figure class="my-meals-item d-flex align-items-center mb-0"><div class="font-weight-500 text-center ml-2">' + mealArray[key].count + '</div><div> <img src="' + mealArray[key].titleImage + '"class="img-wrapper" ></div><div class="info-wrapper pl-4 pr-4 col">' + key + '</div></figure></li>'
+        $(".check-out-meals").append(myMealsList)
     }
+
+    console.log(mealArray)
 
 }
 
@@ -184,6 +185,8 @@ listItemListners = () => {
 
     $("#item").on("click", function (e) {
         selectedDate = e.target.textContent
+        console.log(e.target)
+        $('.list-group-item').removeClass('active-list');
         e.target.className = "list-group-item active-list"
         if (selectedDate) {
             $("#next-button").ready(function () {
@@ -195,8 +198,112 @@ listItemListners = () => {
     $("#next-button").bind("click", function () {
         $("#basic-steps").steps("next");
         $("#delivery-date").append(selectedDate)
+        fetchMeals()
     });
 }
+
+
+fetchMeals = () => {
+    console.log("I am called")
+    let meals = mealCards();
+    for (let i = 0; i < meals.length; i++) {
+        cardTemplate(meals[i])
+    }
+}
+
+
+mealCards = () => {
+
+    let arrayOfItems = [
+        {
+            title: "STEAK PEPPERCORN",
+            sub: "with Sautéed Carrots & French Green Beans",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602724105/production-meal-image-57e49562-348b-42f2-9ca6-9f40a95c8395.jpg",
+        },
+        {
+            title: "CAULIFLOWER SHELL BEEF",
+            sub: "with Nonna’s Soffritto & Italian Cheeses",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1607770959/production-meal-image-7a62bb89-9bcb-4f1f-9f0f-d8372698d48e.jpg",
+        },
+        {
+            title: "PROTEIN-PACKED CHICKEN",
+            sub: "with Mozzarella & Garlicky Broccoli",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602723110/production-meal-image-0a78bae3-2e0c-4ab7-b420-154ed262d4d0.jpg",
+        },
+        {
+            title: "HOMESTYLE CHICKEN",
+            sub: "with Masterful Mac & Cheese",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602721634/production-meal-image-8771fb54-2c23-4063-8007-f5024df074c2.jpg",
+        },
+        {
+            title: "SAUSAGE & PEPPERS",
+            sub: "with Carb Swap Cauliflower Rice",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602723731/production-meal-image-2b1d9d32-e0de-44b4-a2c5-9c7fa22203d5.jpg",
+        },
+        {
+            title: "ZINGY BUFFALO CHICKEN",
+            sub: "with Loaded Cauliflower",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602722020/production-meal-image-1a05b254-2fc5-4eec-9cec-5b1cde753c06.jpg",
+        },
+        {
+            title: "SIERRA CHICKEN BOWL",
+            sub: "with Cilantro-Lime Sauce",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1605813866/production-meal-image-1eab0dae-fb76-4485-9b1b-eaa125cae6c6.jpg",
+        },
+        {
+            title: "OLÉ CHICKEN & SMOKY CHILE SAUCE",
+            sub: "with Veggie Sauté & Sofrito Rice",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602724174/production-meal-image-13231efa-1905-40d4-aca2-208524d1dab1.jpg",
+        },
+        {
+            title: "SAUSAGE BAKED PENNE",
+            sub: "with Sautéed Zucchini & Spinach",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602723731/production-meal-image-2b1d9d32-e0de-44b4-a2c5-9c7fa22203d5.jpg",
+        },
+        {
+            title: "CHICKEN LIVORNO",
+            sub: "with Hearty White Beans & Kale",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602723731/production-meal-image-2b1d9d32-e0de-44b4-a2c5-9c7fa22203d5.jpg",
+        },
+        {
+            title: "WHITE BEAN TURKEY CHILI",
+            sub: "with Cilantro-Lime Rice",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1602722020/production-meal-image-1a05b254-2fc5-4eec-9cec-5b1cde753c06.jpg",
+        },
+        {
+            title: "DUE SOUTH BBQ BEEF",
+            sub: "with Masterful Mac & Cheese",
+            image:
+                "https://res.cloudinary.com/freshly/image/upload/c_scale,w_640/c_crop,h_341,w_512/v1605813866/production-meal-image-1eab0dae-fb76-4485-9b1b-eaa125cae6c6.jpg",
+        },
+    ];
+
+    return arrayOfItems
+
+}
+
+cardTemplate = (mealList) => {
+    let body = document.getElementById('cardMeals');
+    template = document.getElementById("cartMeal")
+    template.content.getElementById("title").textContent = mealList.title;
+    template.content.getElementById("subtitle").textContent = mealList.sub
+    template.content.getElementById("imageCard").src = mealList.image
+    clone = template.content.cloneNode(true)
+    body.appendChild(clone)
+}
+
+
 
 countMonday = (today) => {
     let dayINeed = 1;
